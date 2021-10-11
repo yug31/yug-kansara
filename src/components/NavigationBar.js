460fdb51affbe5a69114ccacb70b81eb232fbd27
ullsee <yugkansara@gmail.com>
2021-10-11 10:58:56 +0530

    change home link

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,11 +5,15 @@ export default function NavigationBar() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Yug Kansara</Navbar.Brand>
+        <Navbar.Brand href="https://yug31.github.io/yug-kansara/">
+          Yug Kansara
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="https://yug31.github.io/yug-kansara/">
+              Home
+            </Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
             <Nav.Link href="/cv">CV</Nav.Link>
             <Nav.Link href="/about">About Me</Nav.Link>
